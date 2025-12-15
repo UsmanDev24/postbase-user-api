e29e3ec8ceb8ec7a4bef4b2e5fa92f8ecd3e2a26
Usman <usman.dev24@gmail.com>
2025-12-15 13:23:20 +0500

    chore: query logs off

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,5 +4,5 @@ import { PrismaClient } from "../generated/prisma/client";
 
 
 const adapter = new PrismaPg({ connectionString:  process.env.DATABASE_URL})
-const prisma = new PrismaClient({ adapter, log: ['query', 'info', 'warn', 'error'], })
+const prisma = new PrismaClient({ adapter, log: ['info', 'warn', 'error'], })
 export { prisma }
